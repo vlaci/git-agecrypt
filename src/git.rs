e6cda5b625d7ad852a40d9b5fd1b2bc8df095580
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-01-06 22:06:39 +0100

    git: reworked testing

--- src/git.rs
+++ src/git.rs
@@ -111,7 +111,7 @@ impl Repository for LibGit2Repository {
 
     fn remove_config(&self, key: &str, value: &str) -> Result<()> {
         if !self.contains_config(key, value) {
-            return Err(Error::NotExist(key.into()));
+            return Err(Error::NotExist(value.into()));
         }
 
         let mut cfg = self.inner.config()?;
@@ -138,84 +138,150 @@ impl Repository for LibGit2Repository {
     }
 
     fn set_config(&self, key: &str, value: &str) -> Result<()> {
+        if let Ok(_) = self.get_config(key) {
+            return Err(Error::AlreadyExists(key.into()));
+        }
         let mut cfg = self.inner.config()?;
-        self.get_config(key)
-            .and_then(|_| Err(Error::AlreadyExists(key.into())))
-            .or_else(|_| {
-                cfg.set_str(key, value)?;
-                Ok(())
-            })
+        cfg.set_str(key, value)?;
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Deref;
+
     use anyhow::Result;
     use assert_fs::prelude::*;
     use assert_fs::TempDir;
     use assert_matches::assert_matches;
     use duct::cmd;
+    use rstest::fixture;
+    use rstest::rstest;
 
     use super::*;
 
-    fn setup_git_repo() -> Result<TempDir> {
-        let dir = TempDir::new()?;
-        cmd!("git", "init").dir(dir.path()).run()?;
-        Ok(dir)
+    #[fixture]
+    fn tempdir() -> TempDir {
+        TempDir::new().unwrap()
     }
 
-    #[test]
-    fn test_repo_can_be_loaded() -> Result<()> {
-        let repo_dir = setup_git_repo()?;
-        let repo = LibGit2Repository::from_dir(repo_dir.path().to_path_buf())?;
+    struct Repo {
+        inner: LibGit2Repository,
+        dir: TempDir,
+    }
 
-        assert_eq!(repo.workdir(), repo_dir.path());
+    impl Deref for Repo {
+        type Target = LibGit2Repository;
+
+        fn deref(&self) -> &Self::Target {
+            &self.inner
+        }
+    }
+
+    #[fixture]
+    fn git_repo(tempdir: TempDir) -> Repo {
+        cmd!("git", "init").dir(tempdir.path()).run().unwrap();
+        let repo = LibGit2Repository::from_dir(tempdir.path().to_path_buf()).unwrap();
+        Repo {
+            inner: repo,
+            dir: tempdir,
+        }
+    }
+
+    #[rstest]
+    fn test_repo_can_be_loaded(git_repo: Repo) -> Result<()> {
+        assert_eq!(git_repo.workdir(), git_repo.dir.path());
+        assert_eq!(git_repo.path(), git_repo.dir.join(".git"));
         Ok(())
     }
 
-    #[test]
-    fn test_repo_required() -> Result<()> {
-        let dir = TempDir::new()?;
-        let repo = LibGit2Repository::from_dir(dir.path().to_path_buf()).err();
+    #[rstest]
+    fn test_repo_required(tempdir: TempDir) -> Result<()> {
+        let repo = LibGit2Repository::from_dir(tempdir.path().to_path_buf()).err();
         assert_matches!(repo, Some(Error::Other(_)));
         Ok(())
     }
 
-    #[test]
-    fn test_bare_repo_is_error() -> Result<()> {
-        let dir = TempDir::new()?;
-        cmd!("git", "init", "--bare").dir(dir.path()).run()?;
-        let repo = LibGit2Repository::from_dir(dir.path().to_path_buf()).err();
+    #[rstest]
+    fn test_bare_repo_is_error(tempdir: TempDir) -> Result<()> {
+        cmd!("git", "init", "--bare").dir(tempdir.path()).run()?;
+        let repo = LibGit2Repository::from_dir(tempdir.path().to_path_buf()).err();
         assert_matches!(repo, Some(Error::Other(_)));
         Ok(())
     }
 
-    #[test]
-    fn test_get_file_contents() -> Result<()> {
-        let repo_dir = setup_git_repo()?;
-
+    #[rstest]
+    fn test_get_file_contents(git_repo: Repo) -> Result<()> {
         let path = PathBuf::from("subdir/file.txt");
         let file_contents = "file contents";
 
-        let repo_file = repo_dir.child(&path);
+        let repo_file = git_repo.dir.child(&path);
         repo_file.touch()?;
         repo_file.write_str(file_contents)?;
-        cmd!("git", "add", &path).dir(repo_dir.path()).run()?;
+        cmd!("git", "add", &path).dir(git_repo.dir.path()).run()?;
         cmd!("git", "commit", "-m", "adding file")
-            .dir(repo_dir.path())
+            .dir(git_repo.dir.path())
             .run()?;
 
-        let repo = LibGit2Repository::from_dir(repo_dir.path().to_path_buf())?;
         assert_eq!(
-            repo.get_file_contents(&repo_dir.join(&path))?,
+            git_repo.get_file_contents(&git_repo.dir.join(&path))?,
             file_contents.as_bytes()
         );
 
         repo_file.write_str("additional_contents")?;
         assert_eq!(
-            repo.get_file_contents(&repo_dir.join(&path))?,
+            git_repo.get_file_contents(&git_repo.dir.join(&path))?,
             file_contents.as_bytes()
         );
+
+        assert!(git_repo
+            .get_file_contents(git_repo.dir.path().parent().unwrap())
+            .is_err());
+
         Ok(())
     }
+
+    #[rstest]
+    fn test_config(git_repo: Repo) -> Result<()> {
+        // At first there are no entries under the "foo" section
+        assert_eq!(git_repo.list_config("foo")?, [] as [String; 0]);
+        assert!(!git_repo.contains_config("foo.bar", "foobar"));
+
+        assert_matches!(git_repo.set_config("invalidkey", "foobar"), Err(_));
+
+        // Both of these are added to the config
+        git_repo.set_config("foo.bar", "foobar")?;
+        git_repo.add_config("foo.bar", "snafu")?;
+
+        // Set won't work with multivalue entries
+        assert_matches!(
+            git_repo.set_config("foo.bar", "FOOBAR"),
+            Err(Error::AlreadyExists(key)) if key == "foo.bar"
+        );
+
+        assert!(git_repo.contains_config("foo.bar", "foobar"));
+        assert!(git_repo.contains_config("foo.bar", "snafu"));
+
+        assert_eq!(git_repo.list_config("foo")?, ["foobar", "snafu"]);
+
+        // Returns the last set config
+        assert_eq!(git_repo.get_config("foo.bar")?, "snafu");
+
+        // Duplicate value cannot be added
+        assert_matches!(
+            git_repo.add_config("foo.bar", "foobar"),
+            Err(Error::AlreadyExists(value)) if value == "foobar"
+        );
+
+        git_repo.remove_config("foo.bar", "foobar")?;
+
+        // Absent value cannot be removed
+        assert_matches!(
+            git_repo.remove_config("foo.bar", "foobar"), Err(Error::NotExist(value)) if value == "foobar"
+        );
+
+        Ok(())
+    }
+
 }
